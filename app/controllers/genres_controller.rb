caa8ddb27307a8d8e3faaf6cd972a719df6163a0
royFloresNyc <royfloresnyc@gmail.com>
2020-08-26 13:53:09 -0400

    Complete songs index, show, and partial forms

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,24 @@
+class GenresController < ApplicationController
+    def index
+    end 
+
+    def show
+    end 
+
+    def new
+    end 
+
+    def create
+    end 
+
+    def edit
+    end
+
+    def update
+    end 
+
+    private
+
+    def form_params
+    end
+end
---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,32 @@
+class SongsController < ApplicationController
+    def index
+        @songs = Song.all
+    end 
+
+    def show
+        @song = Song.find(params[:id])
+    end 
+
+    def new
+        @song = Song.new
+    end 
+
+    def create
+        @song = Song.create(form_params(:name, :artist_id, :genre_id))
+        redirect_to song_path(@song)
+    end 
+
+    def edit
+        @song = Song.find(params[:id])
+    end
+
+    def update
+        @song = Song.find(params[:id])
+    end 
+
+    private
+
+    def form_params(*args)
+        params.require(:song).permit(*args)
+    end
+end
